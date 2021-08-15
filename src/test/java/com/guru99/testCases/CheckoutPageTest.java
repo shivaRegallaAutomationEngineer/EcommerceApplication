509ea7270f4f0f9270eef88549014ef30b044d2d
Shiva regalla Selenium Tutorials <34719991+shivaRegallaAutomationEngineer@users.noreply.github.com>
2021-08-16 00:58:01 +0530

    Update CheckoutPageTest.java

--- src/test/java/com/guru99/testCases/CheckoutPageTest.java
+++ src/test/java/com/guru99/testCases/CheckoutPageTest.java
@@ -3,11 +3,6 @@ package com.guru99.testCases;
 import org.testng.annotations.Test;
 
 import com.guru99.utils.CommonMethods;
-
-
-
-
-
 public class CheckoutPageTest extends CommonMethods{
 	@Test
 	public void Testcase_006() {
@@ -16,21 +11,16 @@ public class CheckoutPageTest extends CommonMethods{
 		CommonMethods.entertext(checkoutpage.Emailadress, "shiva21@gmail.com");
 		CommonMethods.entertext(checkoutpage.Password, "pranitha");
 		CommonMethods.justclick(checkoutpage.Loginbutton);
-		 CommonMethods.justclick(homepage.TV);
-    	 CommonMethods.justclick(homepage.lgtvwishlist);
-    	 
+		CommonMethods.justclick(homepage.TV);
+    	        CommonMethods.justclick(homepage.lgtvwishlist);
 		CommonMethods.justclick(checkoutpage.MyWishlist);
 		CommonMethods.justclick(checkoutpage.Addtocart);
-		
 		CommonMethods.selectname(checkoutpage.countryinest, "United states");
 		CommonMethods.selectname(checkoutpage.Stateinest, "New York");
 		CommonMethods.entertext(checkoutpage.postcode, "542896");
-		
-		
 		CommonMethods.justclick(checkoutpage.Estimate);
 		CommonMethods.justclick(checkoutpage.Shippingcostflatrate);
 		CommonMethods.justclick(checkoutpage.Updatetotal);
-		
 		CommonMethods.justclick(checkoutpage.Checkoutbutton);
 		
 	}
